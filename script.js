1c18287d54bf921457955e2b7f0de9dd3df18768
YodaCoding <92518096+YodaCoding404@users.noreply.github.com>
2024-06-17 10:58:34 +0200

    Add initial cv files

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Hello");
+
+console.log("Hello");
+
+//#region create element
+const element = document.createElement("h1");
+element.textContent = "Hello World!";
+document.body.append(element);
+//#endregion
+
+//#endregion create element
